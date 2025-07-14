e92bdd8de9068497ce57ea31e5f54179334855e4
farahnda <farah.maulida@mhs.unsoed.ac.id>
2025-07-14 13:38:41 +0700

    Hapus file auth test

--- resources/views/livewire/beranda/beranda.blade.php
+++ resources/views/livewire/beranda/beranda.blade.php
@@ -1,41 +1,39 @@
-
 <div id="carouselExampleIndicators" class="carousel slide position-relative carousel-wrapper" data-bs-ride="carousel">
-
-  <!-- Overlay gelap -->
-<div class="position-absolute top-0 start-0 w-100 h-100" style="background-color: rgba(0, 0, 0, 0.6); z-index: 2;"></div>
-  <!-- Teks statis -->
-  <div class="carousel-caption-static text-white">
-    <h1 class="animated-heading">Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu</h1>
-    <a href="#" class="btn-custom animated-heading">Baca Selengkapnya</a>
-  </div>
-
-      <!-- Indicators -->
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-
-  <!-- Carousel Items -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrrgNJDza1Qft2DhirsUmdqYdiMBNR2_Ey98zXcYvNEqC24nGYFmkOu2TeTqCiiOCATmVu5uCW6B4b13g7Da_fPUosOZkt9uqeX5ALS4CcR6C2_61x75WVFdmhVIGag1Is60i-eXw=s1360-w1360-h1020-rw" class="d-block w-100" alt="Slide 1">
+    <!-- Overlay gelap -->
+    <div class="position-absolute top-0 start-0 w-100 h-100" style="background-color: rgba(0, 0, 0, 0.6); z-index: 2;"></div>
+    <!-- Teks statis -->
+    <div class="carousel-caption-static text-white">
+        <h1 class="animated-heading">Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu</h1>
+        <a href="#" class="btn-custom animated-heading">Baca Selengkapnya</a>
     </div>
-    <div class="carousel-item">
-      <img src="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr7KdwcaSyIUYpcJImFSVBTI71DucFBq7nIJLfrKcaJwyTOJ7r92UqHJH4d2PLMNJv9rg9l6DkFoeEtJZT12M7Hl5UsRi9IVO_rnA9ez3M1aqXMr4vf0TYW54qoIXnJZGVJu-CF=s1360-w1360-h1020-rw" class="d-block w-100" alt="Slide 2">
-    </div>
-    <div class="carousel-item">
-      <img src="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqSTcT-uUvrm2HrOobPBUSgdvgZXcM4fyIiR-2Nj8AznRb_-zVwTYnF9FkOTtO3sa4OkqZwecCWgpaHykNiqABVzoav34pGb-w1x2Ezvnad-Z5MqgMPrjw5JqhhBRDQAPeNuKeq=s1360-w1360-h1020-rw" class="d-block w-100" alt="Slide 3">
-    </div>
-  </div>
 
-  <!-- Tombol Navigasi -->
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden"></span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden"></span>
-  </button>
+    <!-- Indicators -->
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    <!-- Carousel Items -->
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrrgNJDza1Qft2DhirsUmdqYdiMBNR2_Ey98zXcYvNEqC24nGYFmkOu2TeTqCiiOCATmVu5uCW6B4b13g7Da_fPUosOZkt9uqeX5ALS4CcR6C2_61x75WVFdmhVIGag1Is60i-eXw=s1360-w1360-h1020-rw" class="d-block w-100" alt="Slide 1">
+        </div>
+        <div class="carousel-item">
+            <img src="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr7KdwcaSyIUYpcJImFSVBTI71DucFBq7nIJLfrKcaJwyTOJ7r92UqHJH4d2PLMNJv9rg9l6DkFoeEtJZT12M7Hl5UsRi9IVO_rnA9ez3M1aqXMr4vf0TYW54qoIXnJZGVJu-CF=s1360-w1360-h1020-rw" class="d-block w-100" alt="Slide 2">
+        </div>
+        <div class="carousel-item">
+            <img src="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqSTcT-uUvrm2HrOobPBUSgdvgZXcM4fyIiR-2Nj8AznRb_-zVwTYnF9FkOTtO3sa4OkqZwecCWgpaHykNiqABVzoav34pGb-w1x2Ezvnad-Z5MqgMPrjw5JqhhBRDQAPeNuKeq=s1360-w1360-h1020-rw" class="d-block w-100" alt="Slide 3">
+        </div>
+    </div>
+
+    <!-- Tombol Navigasi -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden"></span>
+    </button>
 </div>
