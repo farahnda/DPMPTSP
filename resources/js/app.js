77c71c3eeabf47d1ff8259b925541bb3c31c4f15
farahnda <farah.maulida@mhs.unsoed.ac.id>
2025-07-14 13:46:53 +0700

    Add resources/js/app.js

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const isDesktop = window.innerWidth >= 992;
+
+  document.querySelectorAll('.nav-item.dropdown').forEach(function (dropdown) {
+    if (isDesktop) {
+      dropdown.classList.add('dropdown-hover');
+    } else {
+      dropdown.classList.remove('dropdown-hover');
+    }
+  });
+
+  if (!isDesktop) {
+    document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(function (item) {
+      item.addEventListener('click', function () {
+        const dropdown = item.closest('.dropdown');
+        const toggle = dropdown.querySelector('[data-bs-toggle="dropdown"]');
+        const instance = bootstrap.Dropdown.getInstance(toggle);
+        if (instance) {
+          instance.hide();
+        }
+      });
+    });
+  }
+});
